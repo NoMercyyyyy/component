c77feb710a578105b2932f805ed582b6522cad0a
Ayan Ali Khan <alikhanayan05@gmail.com>
2023-12-07 18:49:22 +0530

    Initial commit

---
+++ FormContainer.js
@@ -0,0 +1,36 @@
+const Formcomponent = () => (
+  <div className="absolute top-[903px] left-[0px] bg-white-900 w-[2353px] h-[1059px] text-left text-5xl text-white-900 font-text-md-medium">
+    <img className="absolute top-[931px] left-[0px] w-[2151px] h-[201px]" alt="" src="/vector.svg" />
+    <div className="absolute top-[18px] left-[-33px] bg-darkgreen w-[1955px] h-[932px] overflow-hidden text-109xl text-gainsboro">
+      <div className="absolute top-[676px] left-[178px] tracking-[-0.02em] leading-[30px] font-semibold inline-block w-[165px]">2.5</div>
+      <div className="absolute top-[827px] left-[183px] text-5xl leading-[24px] font-medium text-gray-400 inline-block w-[386px] h-[86px]">
+        <p className="[margin-block-start:0] [margin-block-end:2px]">pounds of food wasted annually</p>
+        <p className="m-0">about 1/2 of food produced</p>
+      </div>
+      <div className="absolute top-[655px] left-[1604px] tracking-[-0.02em] leading-[30px] font-semibold inline-block w-[216px]">203</div>
+    </div>
+    <img className="absolute top-[172px] left-[871px] w-[896px] h-[357px] object-cover" alt="" src="/rectangle-1@2x.png" />
+    <img className="absolute top-[316px] left-[1301px] w-[66px] h-[73px]" alt="" src="/play.svg" />
+    <div className="absolute top-[91px] left-[150px] w-[676px] h-[424px] text-53xl">
+      <b className="absolute top-[92px] left-[0px] tracking-[-0.02em] leading-[80.5px] inline-block w-[676px] h-[159px]">Be a part of the Transformation!</b>
+      <div className="absolute top-[288px] left-[0px] text-9xl leading-[38.5px] font-medium text-gray-400 inline-block w-[676px] h-[136px]">
+        Join Aahaar and make a change by sharing your extra food or discovering affordable, delightful meals near you. Together, we can address food waste and support those in need.
+      </div>
+    </div>
+    <div className="absolute top-[713px] left-[150px] w-[359px] h-[117px] text-17xl">
+      <b className="absolute top-[72px] left-[0px] leading-[30px] inline-block w-[301px]">Trillion</b>
+    </div>
+    <div className="absolute top-[673px] left-[861px] w-[359px] h-[174px] text-gray-400">
+      <div className="absolute top-[154px] left-[11px] leading-[28px] font-medium inline-block w-[459px]">
+        21.7% of the population lack adequate nutrition in India or are affected by hunger
+      </div>
+      <b className="absolute top-[87px] left-[11px] text-13xl leading-[30px] inline-block text-white-900 w-[301px]">Billion</b>
+      <div className="absolute top-[0px] left-[0px] text-109xl tracking-[-0.02em] leading-[30px] font-semibold text-gainsboro inline-block w-[237px]">456</div>
+    </div>
+    <div className="absolute top-[692px] left-[1571px] w-[359px] h-[174px] text-gray-400">
+      <div className="absolute top-[116px] left-[11px] leading-[24px] font-medium inline-block w-[191px]">among 121 nations in the Global Hunger Index</div>
+    </div>
+  </div>
+);
+
+export default Formcomponent;
